7b24dc3ca38105818524c4807e69bd677372e5c1
Shravani Kodam <shravanikodam01>
2025-03-25 20:10:59 -0500

    generating list of restaurants

--- App.js
+++ App.js
@@ -1,3 +1,453 @@
-const heading = React.createElement('h1',{id:"heading"},'hello world from react')
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src='https://masterbundles.com/wp-content/uploads/2023/02/photo-01-355-1.jpg'/>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
+
+const resList = [{
+    "id": "364569",
+    "name": "Chai Point",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/22/cc344c59-ea81-4549-9b6c-e365deb3a163_364569.jpg",
+    "locality": "Indiranagar",
+    "areaName": "Indiranagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+      "Bakery",
+      "Beverages",
+      "Maharashtrian",
+      "Snacks",
+      "Street Food",
+      "South Indian",
+      "Punjabi",
+      "Chaat",
+      "Indian",
+      "American"
+    ],
+    "avgRating": 4.5,
+    "parentId": "1607",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "7.4K+",
+    "sla": {
+      "deliveryTime": 33,
+      "lastMileTravel": 6.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-04-02 00:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹299",
+      "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "4.2",
+        "ratingCount": "914"
+      },
+      "source": "GOOGLE",
+      "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  {
+    "id": "364569",
+    "name": "Chai Point",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/22/cc344c59-ea81-4549-9b6c-e365deb3a163_364569.jpg",
+    "locality": "Indiranagar",
+    "areaName": "Indiranagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+      "Bakery",
+      "Beverages",
+      "Maharashtrian",
+      "Snacks",
+      "Street Food",
+      "South Indian",
+      "Punjabi",
+      "Chaat",
+      "Indian",
+      "American"
+    ],
+    "avgRating": 4.5,
+    "parentId": "1607",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "7.4K+",
+    "sla": {
+      "deliveryTime": 33,
+      "lastMileTravel": 6.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-04-02 00:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹299",
+      "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "4.2",
+        "ratingCount": "914"
+      },
+      "source": "GOOGLE",
+      "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  {
+    "id": "364569",
+    "name": "Chai Point",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/22/cc344c59-ea81-4549-9b6c-e365deb3a163_364569.jpg",
+    "locality": "Indiranagar",
+    "areaName": "Indiranagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+      "Bakery",
+      "Beverages",
+      "Maharashtrian",
+      "Snacks",
+      "Street Food",
+      "South Indian",
+      "Punjabi",
+      "Chaat",
+      "Indian",
+      "American"
+    ],
+    "avgRating": 4.5,
+    "parentId": "1607",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "7.4K+",
+    "sla": {
+      "deliveryTime": 33,
+      "lastMileTravel": 6.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-04-02 00:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹299",
+      "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "4.2",
+        "ratingCount": "914"
+      },
+      "source": "GOOGLE",
+      "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  {
+    "id": "364569",
+    "name": "Chai Point",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/22/cc344c59-ea81-4549-9b6c-e365deb3a163_364569.jpg",
+    "locality": "Indiranagar",
+    "areaName": "Indiranagar",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+      "Bakery",
+      "Beverages",
+      "Maharashtrian",
+      "Snacks",
+      "Street Food",
+      "South Indian",
+      "Punjabi",
+      "Chaat",
+      "Indian",
+      "American"
+    ],
+    "avgRating": 4.5,
+    "parentId": "1607",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "7.4K+",
+    "sla": {
+      "deliveryTime": 33,
+      "lastMileTravel": 6.4,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "6.4 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-04-02 00:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Cafe%20&%20Chai.png"
+              }
+            }
+          ]
+        },
+        "textBased": {
+
+        },
+        "textExtendedBadges": {
+
+        }
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "₹125 OFF",
+      "subHeader": "ABOVE ₹299",
+      "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "lottie": {
+
+        },
+        "video": {
+
+        }
+      }
+    },
+    "reviewsSummary": {
+
+    },
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {
+
+    },
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "4.2",
+        "ratingCount": "914"
+      },
+      "source": "GOOGLE",
+      "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  }]
+
+const RestaurantCard = (props) => {
+    console.log(props)
+    return (
+        <div className='res-card'>
+            <img className="res-logo" src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"+props.resData.cloudinaryImageId}/>
+            <h3>{props.resData.name}</h3>
+            <h4>{props.resData.cuisines.splice(0,4).join(', ')}</h4>
+            <h4>{props.resData.avgRating}</h4>
+            <h4>{props.resData.sla.deliveryTime} Minutes</h4>
+        </div>
+    )
+}
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                {
+                    resList.map((resData)=>{
+                        return <RestaurantCard key= {resData.id} resData={resData}/>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
+root.render(<AppLayout/>)
